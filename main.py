40795ca943748304573161a86b83517c78b3cbc1
MarcosAugusto47 <marcosbsb2014@gmail.com>
2023-10-03 00:52:03 -0300

    Trial to plot a betting historical return

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from analytical_return import (
     minimize_analytical,
@@ -15,29 +16,71 @@ from GameProbs import GameProbs
 from utils import get_bet_return
 
 GAME_ID = "2744178" # Chapecoense x Flamengo
+game_id_list = [
+'5924770',
+ '2939252',
+ '5255267',
+ '5255635',
+ '2921572',
+ '5255194',
+ '4396235',
+ '5255296',
+ '5924901',
+ '2911344',
+ '4698306',
+ '4396233',
+ '4698377',
+ '4698321',
+ '2992793',
+ '5255530',
+ '2939248',
+ '5924787',
+ '2934363',
+ '4698368',
+ '5254905',
+ '5925281',
+ '5924801',
+ '5254935',
+ '5925223'] # Chapecoense x Flamengo
 
 metadata, gameid_to_outcome = load_metadata_artefacts("data/metadata.parquet")
 odds = load_odds("data/odds.parquet")
 
-odds_sample = odds[(odds.GameId==GAME_ID)]
-odds_sample = join_metadata(odds_sample, metadata)
+track_record_list = []
 
-df = GameProbs(GAME_ID).build_dataframe()
+for game_id in game_id_list:
+    
+    print(f"game_id: {game_id}")
+    
+    odds_sample = odds[(odds.GameId==game_id)]
+    odds_sample = join_metadata(odds_sample, metadata)
 
-odds_sample = apply_final_treatment(df_odds=odds_sample, df_real_prob=df)
+    df = GameProbs(game_id).build_dataframe()
 
-odds_favorable = np.array(odds_sample['Odd'])
-real_prob_favorable = np.array(odds_sample['real_prob'])
-event_favorable = list(odds_sample['BetMap'].values)
-solution = minimize_analytical(public_odd=odds_favorable,
-                               real_probabilities=real_prob_favorable,
-                               event=event_favorable,
-                               df_prob=df)
-solution = softmax(solution)
-print(f"solution:\n{solution}")
+    odds_sample = apply_final_treatment(df_odds=odds_sample, df_real_prob=df)
 
-scenario = gameid_to_outcome[GAME_ID]
-print(f"scenario: {scenario}")
+    if len(odds_sample) > 50:
+        continue
 
-financial_return = get_bet_return(df=odds_sample, allocation_array=solution, scenario=scenario)
-print(f"financial_return: {financial_return}")
+    odds_favorable = np.array(odds_sample['Odd'])
+    real_prob_favorable = np.array(odds_sample['real_prob'])
+    event_favorable = list(odds_sample['BetMap'].values)
+    solution = minimize_analytical(public_odd=odds_favorable,
+                                real_probabilities=real_prob_favorable,
+                                event=event_favorable,
+                                df_prob=df)
+    solution = softmax(solution)
+    print(f"solution:\n{solution}")
+
+    scenario = gameid_to_outcome[game_id]
+    print(f"scenario: {scenario}")
+
+    financial_return = get_bet_return(df=odds_sample, allocation_array=solution, scenario=scenario)
+    print(f"financial_return: {financial_return}")
+    
+    track_record = {}
+    track_record['game_id'] = str(game_id)
+    track_record['return'] = financial_return
+    track_record_list.append(track_record)
+
+pd.DataFrame(track_record_list).to_csv("track_record.csv", index=False)
