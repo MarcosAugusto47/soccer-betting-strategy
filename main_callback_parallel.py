fc3aa2692bc66067d263cb9ecaaf2daa6bc16198
MarcosAugusto47 <marcosbsb2014@gmail.com>
2023-12-02 01:41:31 +0000

    more about the baseline + refactoring logic

--- main_callback_parallel.py
+++ main_callback_parallel.py
@@ -1,6 +1,9 @@
+import os
+import argparse
 import numpy as np
 import pandas as pd
 import math
+import datetime
 
 from analytical_return import (
     softmax,
@@ -11,6 +14,11 @@ from data import (
     load_odds,
     join_metadata,
     apply_final_treatment,
+)
+from artifacts import (
+    save_csv_artifact,
+    save_plot_strategy,
+    build_plot_df_wrapper,
 )
 from GameProbs import GameProbs
 from Optimizer import Optimizer
@@ -26,13 +34,15 @@ metadata, gameid_to_outcome = load_metadata_artefacts(config.metadata_path)
 odds = load_odds(config.odds_path)
 odds = join_metadata(odds, metadata)
 
-odds = odds[odds.Datetime.apply(str)<="2019-04-28"]
+odds = odds.sort_values(["Datetime", "GameId"], ascending=True)
 
-def process_group(group: Tuple[str, pd.DataFrame]) -> List[List[Any]]:
+odds = odds[odds.Datetime.apply(str)<"2020-01-01"]
+
+def process_group(group: Tuple[str, pd.DataFrame], args) -> List[List[Any]]:
     
     is_valid_solution = True
 
-    group_name, group_data = group
+    _, group_data = group
    
     games_ids = group_data['GameId'].unique()
     
@@ -41,15 +51,17 @@ def process_group(group: Tuple[str, pd.DataFrame]) -> List[List[Any]]:
     # Initialize dict to store 7x7 matrices/dataframes of real probabilities 
     df_probs_dict = {}
 
-    if len(games_ids) > 1:
+    if len(games_ids) > args.min_games:
 
         for game_id in games_ids:
             df = GameProbs(game_id).build_dataframe()
             
             odds_sample = group_data[(group_data.GameId==game_id)]
             odds_sample = apply_final_treatment(df_odds=odds_sample, df_real_prob=df)
-            odds_sample = filter_by_linear_combination(odds_sample)
-
+            if not args.do_baseline:   
+                odds_sample = filter_by_linear_combination(odds_sample)
+            else:
+                odds_sample = odds_sample.sample(1)
             odds_dict[game_id] = odds_sample
             df_probs_dict[game_id] = df
 
@@ -57,8 +69,8 @@ def process_group(group: Tuple[str, pd.DataFrame]) -> List[List[Any]]:
         print(f"odds_dt.shape: {odds_dt.shape}")
 
         if len(odds_dt) <= config.max_vector_length:
-
-            print(f"Date: {group_name}")
+            iteration_date = odds_dt.Datetime.apply(str).unique()[0]
+            print(f"Date: {iteration_date}")
 
             odds_favorable = np.array(odds_dt['Odd'])
             real_prob_favorable = np.array(odds_dt['real_prob'])
@@ -79,7 +91,7 @@ def process_group(group: Tuple[str, pd.DataFrame]) -> List[List[Any]]:
             
             if any(math.isnan(x) for x in solution):
                 is_valid_solution = False
-
+            print(f"softmax solution: {softmax(solution)})")
             odds_dt['solution'] = softmax(solution)
 
             track_record = []
@@ -95,31 +107,60 @@ def process_group(group: Tuple[str, pd.DataFrame]) -> List[List[Any]]:
                 track_record.append([str(game_id),
                                      financial_return,
                                      len(game_data),
-                                     group_name,
                                      time_limit_flag,
-                                     is_valid_solution])
+                                     is_valid_solution,
+                                     iteration_date])
 
             return track_record
 
 
-def run_strategy():
+def run_strategy(args):
     
     start_time = time()
-    
-    grouped = odds.groupby('Datetime')
+
+    grouped = odds.groupby(args.aggregator)
     
     # Use all available CPU cores for parallel execution
-    num_jobs = -1  
+    num_jobs = 2 
     # Parallelize the group processing
-    results = Parallel(n_jobs=num_jobs)(delayed(process_group)(group) for group in grouped)
+    results = Parallel(n_jobs=num_jobs)(delayed(process_group)(group, args) for group in grouped)
 
     data = [x for x in results if x is not None]
-    flat_list = [item for sublist in data for item in sublist]
-    pd.DataFrame(flat_list).to_csv(config.strategy_result_path, index=False)
+    df_flat = pd.DataFrame([item for sublist in data for item in sublist])
 
+    # Create artefacts folder
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    artefacts_folder = f"artefacts/aggregator{args.aggregator}_min_games{args.min_games}_do_baseline{args.do_baseline}_{timestamp}"
+    os.makedirs(artefacts_folder)
+    args.artefacts_folder = artefacts_folder
+
+    save_csv_artifact(artefacts_folder, "result", df_flat)
+    df_plot = build_plot_df_wrapper(args)
+    save_csv_artifact(artefacts_folder, "result_plot", df_plot)
+    save_plot_strategy(args, df_plot)
+    
     elapsed_time = time() - start_time
     print("Final Elapsed: %.3f sec" % elapsed_time)
 
 
 if __name__ == "__main__":
-    run_strategy()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--aggregator",
+        type=str,
+        help="aggregate by GameId or by Datetime"
+    )
+    parser.add_argument(
+        "--min_games",
+        type=int,
+        default=0,
+        help="threshold of minimum number of games to enter the optimization task"
+    )
+    parser.add_argument(
+        "--do_baseline",
+        action='store_true',
+        help="flag to apply baseline logic or not, not specifying the argument return the opposite of the action"
+    )
+    args = parser.parse_args()
+    print(args)
+    run_strategy(args)
