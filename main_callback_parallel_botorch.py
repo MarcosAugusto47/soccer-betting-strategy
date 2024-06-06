0f3f11d22bc8dad668914e9c7a0a2f03def38649
MarcosAugusto47 <marcosbsb2014@gmail.com>
2024-06-06 15:09:57 -0300

    All changes until masters qualification

--- main_callback_parallel_botorch.py
+++ main_callback_parallel_botorch.py
@@ -5,6 +5,7 @@ import os
 from time import time
 from typing import Any, List, Tuple
 
+import mlflow
 import numpy as np
 import pandas as pd
 import torch
@@ -23,10 +24,9 @@ from data import (
     load_odds,
 )
 from dependencies.config import load_config
-from dependencies.utils import get_bet_return, softmax
+from dependencies.utils import get_bet_return, save_df_as_parquet, softmax
 from filter import filter_by_linear_combination
 from GameProbs import GameProbs
-
 
 config = load_config("config/config.yml")
 
@@ -39,33 +39,40 @@ def setup(args):
 
     odds = odds.sort_values(["Datetime", "GameId"], ascending=True)
 
-    #odds = odds[(odds.Datetime.apply(str)>"2021-05-26")]
-    #odds = odds[(odds.Datetime.apply(str)>"2019-05-01") & (odds.Datetime.apply(str)<"2019-05-05")]
+    odds = odds[(odds.Datetime.apply(str)<"2019-12-08")]
+    
+    # odds = odds[
+    #     (odds.Datetime.apply(str) > "2019-06-01")
+    #     & (odds.Datetime.apply(str) < "2019-07-01")
+    # ]
+
+    # odds = odds[odds]
+
     return odds, gameid_to_outcome
 
 
-def process_group(group: Tuple[str, pd.DataFrame], gameid_to_outcome, args) -> List[List[Any]]:
-    
+def process_group(
+    group: Tuple[str, pd.DataFrame], gameid_to_outcome, args
+) -> List[List[Any]]:
     is_valid_solution = True
 
-    _, group_data = group
-   
-    games_ids = group_data['GameId'].unique()
-    
+    date, group_data = group
+
+    games_ids = group_data["GameId"].unique()
+
     # Initialize dict to store dataframes of favorable bet opportunities
     odds_dict = {}
-    # Initialize dict to store 7x7 matrices/dataframes of real probabilities 
+    # Initialize dict to store 7x7 matrices/dataframes of real probabilities
     df_probs_dict = {}
 
     if len(games_ids) > args.min_games:
-
         for game_id in games_ids:
             df = GameProbs(game_id).build_dataframe()
-            
-            odds_sample = group_data[(group_data.GameId==game_id)]
+
+            odds_sample = group_data[(group_data.GameId == game_id)]
             odds_sample = apply_final_treatment(df_odds=odds_sample, df_real_prob=df)
-            if not args.do_baseline:   
-                odds_sample = filter_by_linear_combination(odds_sample)
+            if not args.do_baseline:
+                odds_sample = filter_by_linear_combination(odds_sample, n=args.bets_per_game, weight=args.weight)
             else:
                 odds_sample = odds_sample.sample(1)
             odds_dict[game_id] = odds_sample
@@ -73,64 +80,74 @@ def process_group(group: Tuple[str, pd.DataFrame], gameid_to_outcome, args) -> L
 
         odds_dt = pd.concat(odds_dict.values())
 
-        #import pdb; pdb.set_trace()
-
-        if len(odds_dt) <= config.max_vector_length and len(odds_dt) > 1:
+        #if len(odds_dt) <= config.max_vector_length and len(odds_dt) > 1:
+        if len(odds_dt) <= config.max_vector_length :
             iteration_date = odds_dt.Datetime.apply(str).unique()[0]
             print(f"Date: {iteration_date}")
 
-            odds_favorable = torch.tensor(np.array(odds_dt['Odd']))
-            real_prob_favorable = torch.tensor(np.array(odds_dt['real_prob']))
-            event_favorable = list(odds_dt['BetMap'].values)
-            games_ids = np.array(odds_dt['GameId'])
-
-            #try:
-            print("Execution of minimization task...")
-
+            odds_favorable = torch.tensor(np.array(odds_dt["Odd"]))
+            real_prob_favorable = torch.tensor(np.array(odds_dt["real_prob"]))
+            event_favorable = list(odds_dt["BetMap"].values)
+            games_ids = np.array(odds_dt["GameId"])
             time_limit_flag = None
 
-            optimizer_instance = BoTorchOptimizer(
-                public_odd=odds_favorable,
-                real_probabilities=real_prob_favorable,
-                event=event_favorable,
-                games_ids=games_ids,
-                df_probs_dict=df_probs_dict
-            )
-            
-            solution = optimizer_instance.run_optimization()
+            if not args.do_baseline:
+                # try:
+                print("Execution of minimization task...")
 
-            print("Finalization of minimization task...")
 
-            #except ValueError:
-                #continue
+                optimizer_instance = BoTorchOptimizer(
+                    public_odd=odds_favorable,
+                    real_probabilities=real_prob_favorable,
+                    event=event_favorable,
+                    games_ids=games_ids,
+                    df_probs_dict=df_probs_dict,
+                )
+
+                solution = optimizer_instance.run_optimization()
+
+                print("Finalization of minimization task...")
+
+                # except ValueError:
+                # continue
+
+                if any(math.isnan(x) for x in solution):
+                    is_valid_solution = False
+                odds_dt["solution"] = softmax(solution)
             
-            if any(math.isnan(x) for x in solution):
-                is_valid_solution = False
-            odds_dt['solution'] = softmax(solution)
+            else:
+                odds_dt["solution"] = 1
+
+            save_df_as_parquet(odds_dt, str(date))
 
             track_record = []
 
-            for game_id, game_data in odds_dt.groupby('GameId', sort=False):
+            for game_id, game_data in odds_dt.groupby("GameId", sort=False):
                 scenario = gameid_to_outcome[game_id]
-                financial_return = get_bet_return(df=game_data,
-                                                  allocation_array=game_data.solution,
-                                                  scenario=scenario)
+                financial_return = get_bet_return(
+                    df=game_data, allocation_array=game_data.solution, scenario=scenario
+                )
 
-                print(f"game_id: {game_id}; financial_return: {np.round(financial_return, 3)}")
+                print(
+                    f"game_id: {game_id}; financial_return: {np.round(financial_return, 3)}"
+                )
 
-                track_record.append([str(game_id),
-                                     financial_return,
-                                     len(game_data),
-                                     odds_dt.n_favorable_bets.values[0],
-                                     time_limit_flag,
-                                     is_valid_solution,
-                                     iteration_date])
+                track_record.append(
+                    [
+                        str(game_id),
+                        financial_return,
+                        len(game_data),
+                        odds_dt.n_favorable_bets.values[0],
+                        time_limit_flag,
+                        is_valid_solution,
+                        iteration_date,
+                    ]
+                )
 
             return track_record
 
 
 def run_strategy(args):
-    
     start_time = time()
 
     odds, gameid_to_outcome = setup(args)
@@ -138,26 +155,45 @@ def run_strategy(args):
     grouped = odds.groupby(args.aggregator)
     
     # Use all available CPU cores for parallel execution
-    num_jobs = 3
+    num_jobs = 1
+    
     # Parallelize the group processing
-    results = Parallel(n_jobs=num_jobs)(delayed(process_group)(group, gameid_to_outcome, args) for group in grouped)
+    results = Parallel(n_jobs=num_jobs)(
+        delayed(process_group)(group, gameid_to_outcome, args) for group in grouped
+    )
 
     data = [x for x in results if x is not None]
     df_flat = pd.DataFrame([item for sublist in data for item in sublist])
 
-    if args.save_experiment:
+    # Start an MLflow experiment
+    with mlflow.start_run():
+        # Log parameters (e.g., settings of the optimizer)
+        mlflow.log_param("aggregator", args.aggregator)
+        mlflow.log_param("min_games", args.min_games)
+        mlflow.log_param("bookmakers", args.bookmakers)
+        mlflow.log_param("bets_per_game", args.bets_per_game)
+        mlflow.log_param("weight", args.weight)
+        mlflow.log_param("do_baseline", args.do_baseline)
 
-        # Create artefacts folder
-        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        artefacts_folder = f"artefacts/botorch_aggregator{args.aggregator}_min_games{args.min_games}_bookmakers{args.bookmakers}_do_baseline{args.do_baseline}_{timestamp}"
-        os.makedirs(artefacts_folder)
-        args.artefacts_folder = artefacts_folder
 
-        save_csv_artifact(artefacts_folder, "result", df_flat)
-        df_plot = build_plot_df_wrapper(args)
-        save_csv_artifact(artefacts_folder, "result_plot", df_plot)
-        save_plot_strategy(args, df_plot)
-    
+        if args.save_experiment:
+            # Create artefacts folder
+            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            artefacts_folder = f"artefacts/{timestamp}"
+            os.makedirs(artefacts_folder)
+            save_csv_artifact(artefacts_folder, "result", df_flat)
+            df_plot = build_plot_df_wrapper(artefacts_folder,  args.aggregator, args.do_baseline)
+            save_csv_artifact(artefacts_folder, "result_plot", df_plot)
+            save_plot_strategy(artefacts_folder, df_plot)
+        
+        mlflow.log_param("timestamp", timestamp)
+        mlflow.log_artifact(f"{artefacts_folder}/result_plot.csv")
+        mlflow.log_artifact(f"{artefacts_folder}/plot.PNG")
+        mlflow.log_metric("wealth", df_plot["stake"].values[-1])
+
+        # End the MLflow run
+        mlflow.end_run()
+
     elapsed_time = time() - start_time
     print("Final Elapsed: %.3f sec" % elapsed_time)
 
@@ -165,31 +201,38 @@ def run_strategy(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--aggregator",
-        type=str,
-        help="aggregate by GameId or by Datetime"
+        "--aggregator", type=str, help="aggregate by GameId or by Datetime"
     )
     parser.add_argument(
         "--min_games",
         type=int,
         default=0,
-        help="threshold of minimum number of games to enter the optimization task"
+        help="threshold of minimum number of games to enter the optimization task",
     )
     parser.add_argument(
         "--bookmakers",
         type=int,
         default=None,
-        help="threshold of minimum number of games to enter the optimization task"
+        help="threshold of minimum number of games to enter the optimization task",
+    )
+    parser.add_argument(
+        "--bets_per_game", type=int, default=5, help="number of bets per game"
+    )
+    parser.add_argument(
+        "--weight",
+        type=float,
+        default=0.5,
+        help="weight of the linear combination filter",
     )
     parser.add_argument(
         "--do_baseline",
-        action='store_true',
-        help="flag to apply baseline logic or not, not specifying the argument return the opposite of the action"
+        action="store_true",
+        help="flag to apply baseline logic or not, not specifying the argument return the opposite of the action",
     )
     parser.add_argument(
         "--save_experiment",
-        action='store_true',
-        help="flag to save the experiment artefacts, not specifying the argument return the opposite of the action"
+        action="store_true",
+        help="flag to save the experiment artefacts, not specifying the argument return the opposite of the action",
     )
     args = parser.parse_args()
     print(args)
