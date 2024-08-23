05644c5a1313dbe53a0b8971211d226c966d95d1
MarcosAugusto47 <marcosbsb2014@gmail.com>
2024-08-22 23:28:05 -0300

    simplifying Optimizer.py and setting COBYLA

--- Optimizer.py
+++ Optimizer.py
@@ -1,7 +1,6 @@
-import numpy as np
 import warnings
-
 from time import time
+
 from scipy.optimize import minimize
 
 
@@ -10,48 +9,46 @@ class TookTooLong(Warning):
 
 
 class Optimizer:
-
     def __init__(self, maxtime_sec=120):
         self.nit = 0
         self.maxtime_sec = maxtime_sec
-        self.flag=False
+        self.flag = False
 
     def callback(self, x):
         # callback to terminate if maxtime_sec is exceeded
         self.nit += 1
         elapsed_time = time() - self.start_time
-        
-        if elapsed_time > self.maxtime_sec:
 
-            self.flag=True
-            
-            warnings.warn("Terminating optimization: time limit reached",
-                          TookTooLong)
-            
-        #else:
-            #print("Elapsed: %.3f sec" % elapsed_time)
-            #print("Elapsed iterations: ", self.nit)
+        if elapsed_time > self.maxtime_sec:
+            self.flag = True
+
+            warnings.warn("Terminating optimization: time limit reached", TookTooLong)
+
+        # else:
+        # print("Elapsed: %.3f sec" % elapsed_time)
+        # print("Elapsed iterations: ", self.nit)
 
     def run_optimization(self, fun, x0, args, bounds=None):
         self.start_time = time()
-        
-        # Constraint function
-        def constraint_equation(x):
-            # This function returns 0 when the sum of x equals 1
-            return np.sum(x[1:]) - 1
 
-        # Constraint dictionary
-        constraint = {'type': 'eq', 'fun': constraint_equation}
+        # # Constraint function
+        # def constraint_equation(x):
+        #     # This function returns 0 when the sum of x equals 1
+        #     return sum(x) - 1
 
-        res = minimize(fun=fun,
-                       x0=x0,
-                       args=args,
-                       bounds=bounds,
-                       callback=self.callback,
-                       tol=0.01,
-                       method='Powell',
-                       #constraints=constraint,
-                       options={"disp": True},
+        # # Constraint dictionary
+        # constraint = {'type': 'eq', 'fun': constraint_equation}
+
+        res = minimize(
+            fun=fun,
+            x0=x0,
+            args=args,
+            bounds=bounds,
+            # callback=self.callback,
+            tol=0.001,
+            method="COBYLA",
+            # constraints=constraint,
+            options={"disp": True},
         )
-        
-        return res.x, self.flag
+
+        return res.x, res.fun, self.flag
